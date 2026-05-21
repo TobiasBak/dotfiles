5f9527c97b72b056303caa7ac34e69e940f64daa
TobiasBak <tobiasbak@live.dk>
2026-05-21 14:57:51 +0200

    Add private Tailscale NAS share

--- nixos/hosts/laptop-server/configuration.nix
+++ nixos/hosts/laptop-server/configuration.nix
@@ -20,6 +20,37 @@
     openFirewall = false;
   };
 
+  services.samba = {
+    enable = true;
+    openFirewall = false;
+    settings = {
+      global = {
+        "server string" = "laptop-server";
+        "workgroup" = "WORKGROUP";
+        "security" = "user";
+        "map to guest" = "Never";
+        "hosts allow" = "100.64.0.0/10 127.0.0.1";
+        "hosts deny" = "0.0.0.0/0";
+        "smb encrypt" = "required";
+      };
+      nas = {
+        "path" = "/srv/nas";
+        "browseable" = "yes";
+        "read only" = "no";
+        "valid users" = "tobias";
+        "force user" = "tobias";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+    };
+  };
+
+  networking.firewall.interfaces.tailscale0.allowedTCPPorts = [ 445 ];
+
+  systemd.tmpfiles.rules = [
+    "d /srv/nas 0755 tobias users -"
+  ];
+
   console.keyMap = "dk";
 
   users.users.tobias = {
