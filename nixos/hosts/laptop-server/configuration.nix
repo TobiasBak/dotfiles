9001ff102e16744a391894fb27f07e727c6d70e4
TobiasBak <tobiasbak@live.dk>
2026-06-01 17:21:16 +0200

    Keep laptop-server LAN and Tailscale access

--- nixos/hosts/laptop-server/configuration.nix
+++ nixos/hosts/laptop-server/configuration.nix
@@ -12,24 +12,26 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
   services.xserver.xkb.layout = "dk";
   services.xserver.desktopManager.xfce.enable = true;
   services.xrdp = {
     enable = true;
     defaultWindowManager = "xfce4-session";
-    openFirewall = false;
+    openFirewall = true;
   };
 
   services.samba = {
     enable = true;
-    openFirewall = false;
+    openFirewall = true;
     settings = {
       global = {
         "server string" = "laptop-server";
         "workgroup" = "WORKGROUP";
         "security" = "user";
         "map to guest" = "Never";
-        "hosts allow" = "100.64.0.0/10 127.0.0.1";
+        "invalid users" = [ "root" ];
+        "hosts allow" = "100.64.0.0/10 192.168.86.0/24 127.0.0.1";
         "hosts deny" = "0.0.0.0/0";
         "smb encrypt" = "required";
       };
@@ -37,7 +39,7 @@
         "path" = "/srv/nas";
         "browseable" = "yes";
         "read only" = "no";
-        "valid users" = "tobias";
+        "valid users" = [ "tobias" ];
         "force user" = "tobias";
         "create mask" = "0644";
         "directory mask" = "0755";
@@ -45,16 +47,23 @@
     };
   };
 
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  networking.firewall.allowedTCPPorts = [ 22 3389 ];
   networking.firewall.interfaces.tailscale0.allowedTCPPorts = [ 445 3389 ];
 
   systemd.tmpfiles.rules = [
     "d /srv/nas 0755 tobias users -"
   ];
 
-  console.keyMap = "dk";
+  console.keyMap = "dk-latin1";
 
   users.users.tobias = {
     isNormalUser = true;
+    description = "tobias";
     extraGroups = [ "wheel" "networkmanager" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFEvr2qCdxh7peyDqmauJKmLiql3e77uo8+IrkmSwRDe tobias@windows"
